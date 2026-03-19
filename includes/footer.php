3ddc06daf931659f32db776229b0da52f753e877
Jericho Bellen <jerichopbellen@gmail.com>
2026-03-19 09:39:08 +0800

    removed not working links

--- includes/footer.php
+++ includes/footer.php
@@ -4,12 +4,5 @@
       <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
         <span class="text-muted">&copy; <?= date('Y') ?> Handsforth. All rights reserved.</span>
       </div>
-      <div class="col-md-6 text-center text-md-end">
-        <a href="#" class="text-muted me-3"><i class="bi bi-facebook"></i></a>
-        <a href="#" class="text-muted me-3"><i class="bi bi-twitter"></i></a>
-        <a href="#" class="text-muted me-3"><i class="bi bi-instagram"></i></a>
-        <a href="#" class="text-muted"><i class="bi bi-envelope"></i></a>
-      </div>
-    </div>
   </div>
 </footer>
